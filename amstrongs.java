4272f616b175a78cf3346962b7df32dd553b474e
Aksharakeerthi <akshara2223@gmail.com>
2017-07-12 17:26:23 +0530

    Update and rename amstrong.java to amstrongs.java

---
+++ amstrongs.java
@@ -0,0 +1,22 @@
+# amstrongs
+import java.io.*;
+import java.util.*;
+public class amstrong{
+public static void main(String args[]){
+int m=0,k,h,temp;
+temp=h;
+Scanner sc=new Scanner(System.in);
+System.out.println("Enter the number");
+h=sc.nextInt();
+while(h>0){
+k=h%10;
+h=h/10;
+m=m+(k*k*k);
+}
+if(temp==m){
+System.out.println("The number is Amstrong");
+}
+else
+System.out.println("The number is not a Amstrong");
+}
+}
